6467a60301925df212d1fe64b469b9bcdf31b462
Jonas <publik.froeller.jonas@gmx.at>
2024-08-03 14:39:30 +0200

    removes boilerplate in `app.vue`

--- app.vue
+++ app.vue
@@ -1,6 +1,26 @@
 <template>
-  <div>
+  <div id="ui-content">
+    <header>Header</header>
+
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <main>Main</main>
+
+    <footer>Footer</footer>
   </div>
 </template>
+
+<style scoped>
+header {
+  position: sticky;
+  top: 0;
+  z-index: 999;
+}
+
+#ui-content {
+  position: relative;
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+  min-height: 100vh;
+  min-height: 100dvh;
+}
+</style>
