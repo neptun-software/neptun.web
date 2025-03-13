d56595482edccacc944ea75154d53ff2fb1d2c8b
Jonas <publik.froeller.jonas@gmx.at>
2025-03-13 13:20:50 +0100

    lints codebase

--- composables/useUiStore.ts
+++ composables/useUiStore.ts
@@ -5,14 +5,14 @@ export function useUiStore() {
   const router = useRouter()
 
   const isZenMode = computed(() => route.query.layout === 'zen' && (route.name === 'Dashboard' || route.path === '/'))
-  const toggleZenMode = () => {
+  const toggleZenMode = async () => {
     const query = { ...route.query }
     if (isZenMode.value) {
       delete query.layout
     } else {
       query.layout = 'zen'
     }
-    router.replace({ query })
+    await router.replace({ query })
   }
 
   return {
