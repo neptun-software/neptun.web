679f827f5e1ab10ebf52b3be39a7ec340a20e236
Jonas <publik.froeller.jonas@gmx.at>
2025-03-20 18:58:26 +0100

    adds imprint (closes #213)

--- components/sections/layout/DynamicFooter.vue
+++ components/sections/layout/DynamicFooter.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import type { HTMLAttributes } from 'vue'
 import { cn } from '@/lib/utils'
+import { Icon } from '@iconify/vue'
 
 const props = defineProps<{
   class?: HTMLAttributes['class']
@@ -10,11 +11,44 @@ const props = defineProps<{
 <template>
   <footer :class="cn('flex z-10 flex-col border-t bg-background p-8 justify-center', props.class)">
     <AppLinks />
+
+    <ShadcnSeparator class="mt-2 mb-1 bg-transparent" />
+
+    <div class="flex flex-wrap gap-4 justify-center mt-4">
+      <NuxtLink to="https://github.com/neptun-software" target="_blank" class="flex gap-2 items-center text-muted-foreground hover:text-foreground">
+        <Icon icon="ant-design:github-filled" class="w-5 h-5" />
+        <span>GitHub</span>
+      </NuxtLink>
+      <NuxtLink to="https://huggingface.co/neptun-org" target="_blank" class="flex gap-2 items-center text-muted-foreground hover:text-foreground">
+        <Icon icon="mingcute:ai-line" class="w-5 h-5" />
+        <span>Huggingface</span>
+      </NuxtLink>
+      <NuxtLink to="https://pypi.org/project/neptun" target="_blank" class="flex gap-2 items-center text-muted-foreground hover:text-foreground">
+        <Icon icon="akar-icons:python-fill" class="w-5 h-5" />
+        <span>CLI-Tool</span>
+      </NuxtLink>
+      <NuxtLink to="https://github.com/apps/neptun-github-app" target="_blank" class="flex gap-2 items-center text-muted-foreground hover:text-foreground">
+        <Icon icon="gg:data" class="w-5 h-5" />
+        <span>GitHub App</span>
+      </NuxtLink>
+      <NuxtLink to="https://neptun-tools-docs.pages.dev" target="_blank" class="flex gap-2 items-center text-muted-foreground hover:text-foreground">
+        <Icon icon="oui:documentation" class="w-5 h-5" />
+        <span>Documentation</span>
+      </NuxtLink>
+      <NuxtLink to="/imprint" active-class="text-primary" class="flex gap-2 items-center text-muted-foreground hover:text-foreground">
+        <Icon icon="carbon:information" class="w-5 h-5" />
+        <span>Imprint</span>
+      </NuxtLink>
+    </div>
+
+    <ShadcnSeparator class="mt-2 mb-1 bg-transparent" />
+    
     <p class="mt-2 text-center text-muted-foreground">
       Animated Icons by
       <NuxtLink
         class="hover:underline"
-        to="https://useanimations.com/"
+        to="https://useanimations.com"
+        target="_blank"
         external
       >
         useanimations
---
+++ pages/imprint.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+definePageMeta({
+  name: 'Imprint',
+  alias: ['/imprint'],
+})
+</script>
+
+<template>
+  <div class="mx-8 mt-6 mb-4">
+    <div class="block mx-auto prose">
+      <h1>Imprint</h1>
+
+      <p>
+        <strong>Jonas Fr&ouml;ller</strong><br />
+        Limesstra&szlig;e 12/14<br />
+        4060 Leonding, Austria
+      </p>
+
+      <h2>Contact</h2>
+      <p>
+        <b>Phone</b>: (0732) 67 33 68 &ndash; 0<br />
+        <b>E-Mail</b>: <a href="mailto:neptunai.contact@gmail.com">neptunai.contact@gmail.com</a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
